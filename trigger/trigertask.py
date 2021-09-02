9efbbe6f5f4f7956483c0d48c744235a8d84bcd1
holand76 <thegreath76@gmail.com>
2021-09-02 20:27:56 -0300

    troquei trigger para py

--- trigger/trigertask.py
+++ trigger/trigertask.py
@@ -1,4 +1,18 @@
 import subprocess
 import time
-run = subprocess.run(' cd /home/cremoso/Git/covid_cg-br/Covid-cg-br/COVID; scrapy crawl covid -o covid.json', shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-print(run)
+from datetime import datetime
+
+
+
+while True:
+
+    data_HJ = datetime.now()
+    hora = data_HJ.hour
+    minute = data_HJ.minute
+    if hora == 20 and minute == 27:
+        run = subprocess.run(' cd /home/cremoso/Git/covid_cg-br/Covid-cg-br/COVID; scrapy crawl covid -o covid.json', shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+        print(run)
+
+        time.sleep(70)
+
+

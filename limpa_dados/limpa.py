600878f060d9d20128101344dfb084d4f0b94f29
holand76 <thegreath76@gmail.com>
2021-10-13 17:54:23 -0300

    limpeza de dados feita

--- limpa_dados/limpa.py
+++ limpa_dados/limpa.py
@@ -1,5 +1,5 @@
 import ast
-
+import time
 
 
 with open ('covid.json','r') as file:
@@ -7,6 +7,10 @@ with open ('covid.json','r') as file:
 
 
 tamanho = len(a)
+print(type(a))
+print('tipo')
+
+lista_contendoOsDicionarios = []
 
 for index in range(tamanho):
     #print(a[index])
@@ -15,14 +19,16 @@ for index in range(tamanho):
     
 
     try:
-
+    
         transforma_paradict = ast.literal_eval(a[index])
-
+        #print(transforma_paradict,"\n")
+#        time.sleep(1) #pra debug
+        
     except:
         continue
 
     for key in transforma_paradict:
-        lista_auxiliar = []
+        lista_auxiliar = [] #para editar as strings
 
         for linha in transforma_paradict[key]: 
 
@@ -37,12 +43,21 @@ for index in range(tamanho):
             
             lista_auxiliar.append(c)
 #           print(c,'limpo')
+        
         transforma_paradict[key] = [lista_auxiliar]
+        #print('resultado final ')
+        #print(transforma_paradict)
+        #print(type(transforma_paradict))
+    #    for i in transforma_paradict:
+            #print(i)
+    #time.sleep(1)
+    print(transforma_paradict)
+    lista_contendoOsDicionarios.append(transforma_paradict)
 
-        print(transforma_paradict)
-   
-
-
+with open('dados.py','w') as arquivo:
+    string = str(lista_contendoOsDicionarios) #so da pra escrevers strings nos arquivos
+    string = 'listaCOVID = '+string 
+    arquivo.write(string)
 
 #for i in a:
  #   if a in ('][\n','[\n'):

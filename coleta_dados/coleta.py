a2ccf33db8ba3ab8ccd83c2f16059d0e23923962
Mr.76 <bartmoss77@tutanota.com>
2022-12-17 10:44:13 -0300

    change shadowing

--- coleta_dados/coleta.py
+++ coleta_dados/coleta.py
@@ -11,11 +11,11 @@ def cria_dict(data,lista_texto_valores,lista_valores):
         dicionario_para_json[item.text] = lista_valores[i]
     return dicionario_para_json
 
-def load_dados_json(nome_file,dicionario_web): 
-    with open(nome_file,'r') as file:
+def load_dados_json(nome_arquivo,dicionario_web): 
+    with open(nome_arquivo,'r') as file:
         lista_dict_total = json.load(file)
     lista_dict_total.append(dicionario_web)    
-    escreve_lista_js(lista_dict_total,nome_file)
+    escreve_lista_js(lista_dict_total,nome_arquivo)
 
 
 def escreve_lista_js(lista_dicionario,nome):
@@ -25,7 +25,7 @@ def escreve_lista_js(lista_dicionario,nome):
 
 
 
-nome_file = "dados.json"
+file_name = "dados.json"
 DATA_TODAY = datetime.now() 
 DATA_TODAY = ("{}/{}/{}").format(DATA_TODAY.day,DATA_TODAY.month,DATA_TODAY.year)
 parser = 'html.parser'
@@ -33,10 +33,12 @@ pagina_web = requests.get('https://campinagrande.pb.gov.br/')
 pagina_soup = BeautifulSoup(pagina_web.text,parser)
 lista_texto_web = pagina_soup.find_all("p",class_="title")
 lista_valores_web = pagina_soup.find_all("p",class_="count")
+
+
 for index in range(len(lista_texto_web)):
     string_to_int = str(lista_valores_web[index].text.replace("%",""))
     lista_valores_web[index] = int(string_to_int)
 
 
 dicionario_json = cria_dict(DATA_TODAY,lista_texto_web,lista_valores_web)
-load_dados_json(nome_file,dicionario_json)
+load_dados_json(file_name,dicionario_json)
